0e0716832616a8e9b1bf40707800ef5f4568a4d8
AHMED ALI MUGHAL <163463244+coderAAM@users.noreply.github.com>
2025-01-11 00:11:49 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " --> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.addFirst("a");
+        m.addFirst("is");
+        m.addFirst("This");
+        m.printList();
+        m.printList();
+        m.addLast("list");
+        m.printList();
+    }
+}
